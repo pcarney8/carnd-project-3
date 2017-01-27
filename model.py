81d1d8a9bc3937d2d5c181d2c6abad65022e29bc
Patrick Carney <patrick.s.carney@gmail.com>
2017-01-27 02:56:59 +0000

    shifting test.csv to linux, model updates

--- model.py
+++ model.py
@@ -1,27 +1,40 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Convolution2D, MaxPooling2D, Dropout
 import csv
+import numpy as np
+from scipy import ndimage
+import cv2
+from sklearn.utils import shuffle
 
 # Load in the center camera data and steering angle
-# TODO CHANGE THE INPUT PIXEL SIZING OR CHANGE THE MODEL INPUT SIZING
-# images are currently 320x160
-X_input = list()
-y_input = list()
+X_train = list()
+y_train = list()
 
-with open('test.csv', 'rb') as csvfile:
+with open('test.csv', 'rt') as csvfile:
 	csvreader = csv.reader(csvfile)
 	for row in csvreader:
-		X_input.append(row[0])
-		y_input.append(row[3])
+		X_train.append(row[0])
+		y_train.append(row[3].strip())
 
-print(y_input)
-print(X_input)
+print(y_train)
+print(X_train)
+
+# resize images, currently 320x160
+x_train = list()
+
+for img in X_train:
+	read_image = cv2.imread(img)
+	resized_image = cv2.resize(read_image, (32, 16))
+	x_train.append(resized_image)
+
+print(x_train[0].shape)
 
 # shuffle the data
+x_train, y_train = shuffle(x_train, y_train)
 
 # normalize the features ? do i need to do this?
 
-# one hot
+# one hot pretty sure i don't need one hot because i only have one value
 
 # Split into training and validation data sets, model.fit can do this for me
 
@@ -43,12 +56,13 @@ print(X_input)
 #model.add(Dense(43))
 
 # Compile and train the model
-#model.compile('adam', 'categorical_crossentropy', ['accuracy'])
+model.compile('adam', 'categorical_crossentropy', ['accuracy'])
 #history = model.fit(X_normalized, y_one_hot, batch_size=128, nb_epoch=10, validation_split=0.2)
+history = model.fit(X_normalized, batch_size=128, nb_epoch=1, validation_split=0.15)
 
 # Save model
-#with open("model.json", "w") as model_file:
-#	print(model.to_json(), file=model_file)
+with open("model.json", "w") as model_file:
+	print(model.to_json(), file=model_file)
 
 # Save weights
-#model.save_weights('model.h5')
+model.save_weights('model.h5')
